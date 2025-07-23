e43745fcf90f84e650166066c8e5dc816f09d99e
Yazan TAYAN <yazan.tayan@msaaq.com>
2025-07-23 23:48:54 +0300

    fix: remove duplicate UserCreated event dispatch from EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\UserCreated;
-use App\Listeners\CreateWalletForUser;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -14,9 +12,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        UserCreated::class => [
-            CreateWalletForUser::class,
-        ],
+      //
 
     ];
 }
