63f132ac3f77d0dd3fa6e27cd74d7865281f4c3b
Yazan TAYAN <yazan.tayan@msaaq.com>
2025-07-24 13:20:18 +0300

    chore: cs

--- app/Http/Controllers/Api/V1/DepositController.php
+++ app/Http/Controllers/Api/V1/DepositController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\Api\V1\LoginRequest;
-use App\Http\Requests\Api\V1\RegisterRequest;
 use App\Http\Requests\Api\V1\StoreDepositRequest;
 use App\Http\Resources\Api\V1\DepositResource;
-use App\Http\Resources\Api\V1\UserResource;
 use App\Models\User;
 use App\Models\Wallet;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 class DepositController extends Controller
 {
