863521141af7d94fff4ae3f0262f1ebb8b56374b
Yazan TAYAN <yazan.tayan@msaaq.com>
2025-07-24 08:57:09 +0300

    chore: add throttle to log in route

--- routes/api/V1/index.php
+++ routes/api/V1/index.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Api\V1\AuthController;
 
 Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
-    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])
+        ->middleware('throttle:5,1')
+        ->name('login');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('me', [AuthController::class, 'me'])->name('me');
