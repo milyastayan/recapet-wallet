2691bf700a5ceb7e85c26dbbdc88f7c09cfc6b06
Yazan TAYAN <yazan.tayan@msaaq.com>
2025-07-24 09:58:53 +0300

    chore: make the user unique in wallet table

--- database/migrations/2025_07_23_073644_create_wallets_table.php
+++ database/migrations/2025_07_23_073644_create_wallets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->uuid()->unique();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->unique();
             $table->integer('balance')->default(0);
             $table->timestamps();
         });
